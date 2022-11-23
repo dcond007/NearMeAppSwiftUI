7e003acfc12b0349186d601be9dd49839f7fc628
Daniel Condly <onyxmatrix24@gmail.com>
2022-11-23 17:56:28 -0500

    Initial Commit

---
+++ NearMeAppSwiftUI/NearMeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeAppSwiftUIApp.swift
+//  NearMeAppSwiftUI
+//
+//  Created by Daniel Condly on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct NearMeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
